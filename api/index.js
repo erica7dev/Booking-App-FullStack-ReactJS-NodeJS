9f6b64d23cdbedd9191c22c3521bf294fc76e337
erica7dev <erikaisa7dev@gmail.com>
2022-07-27 14:20:10 -0300

    add routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './db/conn.js'
+import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
 
 dotenv.config()
 const PORT = process.env.PORT || 8000
@@ -8,8 +12,13 @@ const mode = process.env.NODE_ENV
 
 const app = express()
 
-connectDB()
+//middlewares
+app.use('/api/auth', authRoute)
+app.use('/api/users', usersRoute)
+app.use('/api/hotels', roomsRoute)
+app.use('/api/rooms', hotelsRoute)
 
 app.listen(8000, () => {
+  connectDB()
   console.log('Server is running on port 8000')
 })
